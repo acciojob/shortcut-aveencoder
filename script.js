ffb8818e2f779bc7c305bfb51601198e953f6271
aveencoder <aveen598@gmail.com>
2025-07-03 13:17:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function shortcut(s1, s2) {
   // Guard: if either is null, undefined, or empty
-  if (!s1 || !s2) return "";
+  // if (!s1 || !s2) return "";
 
   // Safe to access first characters
   const firstChar = s1[0];
   const secondChar = s2[0];
+	console.log("firstChar", firstChar)
   return `${firstChar}${secondChar}`;
 }
 
 
-Do not change the code below.
+// Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
