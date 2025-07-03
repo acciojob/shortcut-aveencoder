9045ab981522db2e85c56d624f4c1a8b9f452f4a
aveencoder <aveen598@gmail.com>
2025-07-03 12:49:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function shortcut(s1, s2) {
-  // your code here
+ let firstChar = s1[0];
+  let secondChar = s2[0];
+ 
+  if(!(s1.length)||!(s2.length)) return "";
+   
+  return`'${firstChar}${secondChar}'`;
+	
 }
 
-// Do not change the code below.
+Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
